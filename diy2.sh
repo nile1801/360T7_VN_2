15b7544c4f919cabf2e40bb43e0e7f4f514d7733
nile1801 <ducati1801@gmail.com>
2024-05-13 16:04:14 +0700

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -20,6 +20,10 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
-sed -i "s/hostname='ImmortalWrt'/hostname='360T7'/g" package/base-files/files/bin/config_generate
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By Xiaw1024 $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i "s/timezone='UTC'/timezone='Asia\/Ho_Chi_Minh'/g" package/base-files/files/bin/config_generate
+sed -i 's/time1.apple.com/0.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
+sed -i 's/time1.google.com/1.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
+sed -i 's/time.cloudflare.com/2.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
+sed -i 's/pool.ntp.org/3.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
+sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By NILE $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
