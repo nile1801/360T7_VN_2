46cfb5a3aeb4aa97f8555eb6b88c2796838e21bb
nile1801 <ducati1801@gmail.com>
2024-05-14 00:24:08 +0700

    Update diy2.sh

--- diy2.sh
+++ diy2.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
-###
- # @Author: xiawang1024
- # @Date: 2023-01-06 15:29:51
- # @LastEditTime: 2023-01-06 15:30:17
- # @LastEditors: xiawang1024
- # @Description: 
- # @FilePath: /OpenWrt-360T7/diy2.sh
- # 工作，生活，健康
-### 
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-sed -i "s/timezone='UTC'/timezone='Asia\/Ho_Chi_Minh'/g" package/base-files/files/bin/config_generate
+sed -i "s/UTC/Asia/Ho_Chi_Minh/" package/base-files/files/bin/config_generate
 sed -i 's/time1.apple.com/0.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
 sed -i 's/time1.google.com/1.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
 sed -i 's/time.cloudflare.com/2.asia.pool.ntp.org/g'  package/base-files/files/bin/config_generate
